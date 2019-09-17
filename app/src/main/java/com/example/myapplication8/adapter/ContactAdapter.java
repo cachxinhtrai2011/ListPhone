ad6684ce4c0274b054255fa1fd2d6dfacb42032f
cachxinhtrai2011 <ltt.9a1.dtthuan@gmail.com>
2019-09-17 16:43:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication8/adapter/ContactAdapter.java
@@ -0,0 +1,68 @@
+package com.example.myapplication8.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.myapplication8.R;
+import com.example.myapplication8.model.Contact;
+
+import java.util.List;
+
+
+    public class ContactAdapter extends ArrayAdapter<Contact>{
+
+        private Context context;
+        private int resource;
+        private List<Contact> arrayContact;
+
+        public ContactAdapter(Context context, int resource, List<Contact> objects) {
+            super(context, resource, objects);
+            this.context =context;
+            this.resource=resource;
+            this.arrayContact=objects;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if(convertView == null){
+                viewHolder = new ViewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.item_contact_listview,parent,false);
+                viewHolder.imgAvatar = (ImageView) convertView.findViewById(R.id.img_avatar);
+                viewHolder.tvName = (TextView) convertView.findViewById(R.id.tv_name);
+                viewHolder.tvNumber = (TextView) convertView.findViewById(R.id.tv_number);
+
+                convertView.setTag(viewHolder);
+
+            }else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            Contact contact = arrayContact.get(position);
+
+            viewHolder.tvName.setText(contact.getmName());
+            viewHolder.tvNumber.setText(contact.getmNumber());
+
+            if(contact.isMale()){
+                viewHolder.imgAvatar.setBackgroundResource(R.drawable.ic_male);
+            }else {
+                viewHolder.imgAvatar.setBackgroundResource(R.drawable.ic_female);
+            }
+
+            return convertView;
+        }
+        public class ViewHolder{
+            ImageView imgAvatar;
+            TextView tvName;
+            TextView tvNumber;
+        }
+    }
+
